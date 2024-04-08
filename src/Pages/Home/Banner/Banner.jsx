bf30b946dd5a8d57d70ff3ecd788524520ce0a34
ProgSomel <somelahmed88@gmail.com>
2024-04-08 14:06:17 +0600

    Banner section done

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import banner1 from "../../../assets/images/banner/banner1.jpg";
+import banner2 from "../../../assets/images/banner/banner2.jpg";
+import banner3 from "../../../assets/images/banner/banner3.jpg";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+
+// Import Swiper styles
+import "swiper/css";
+
+const Banner = () => {
+  return (
+    <div className="my-8">
+      <AwesomeSlider>
+        <div><img src={banner1} alt="" /></div>
+        <div><img src={banner2} alt="" /></div>
+        <div><img src={banner3} alt="" /></div>
+      </AwesomeSlider>
+    </div>
+  );
+};
+
+Banner.propTypes = {};
+
+export default Banner;
