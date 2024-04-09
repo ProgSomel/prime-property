1fe9f8635a78425b8edbc94b9cabbd21fc874c43
ProgSomel <somelahmed88@gmail.com>
2024-04-09 06:56:30 +0600

    implement AOS Pakage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,33 @@
+import  { useEffect } from "react";
 import EstatesCard from "../EstatesCard/EstatesCard";
 import Banner from "./Banner/Banner";
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
+import "aos/dist/aos.css";
+import AOS from "aos"; 
 
 const Home = () => {
-    const estates = useLoaderData();
-    console.log(estates);
-    return (
-        <div>
-            <Banner></Banner>
-            <div className="grid grid-cols-1 md:grid-cols-2 max-w-6xl mx-auto gap-8 my-12 px-2 md:px-4 lg:px-8 min-h-screen">
-                {
-                    estates.map(estate => <EstatesCard key={estate.id} estates={estate}></EstatesCard>)
-                }
-            </div>
-        </div>
-    );
+  const estates = useLoaderData();
+  
+  // Initialize AOS
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with your desired options
+  }, []);
+
+  return (
+    <div>
+      <Banner></Banner>
+      <div
+        data-aos="fade-right"
+        className="grid grid-cols-1 md:grid-cols-2 max-w-6xl mx-auto gap-8 my-12 px-2 md:px-4 lg:px-8 min-h-screen"
+      >
+        {estates.map((estate) => (
+          <EstatesCard key={estate.id} estates={estate}></EstatesCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-
-Home.propTypes = {
-
-};
-
+Home.propTypes = {};
 
 export default Home;
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,10 +1,24 @@
 import {Outlet} from "react-router-dom"
 import Navbar from "../Pages/Navbar/Navbar";
 import Footer from "../Pages/Footer/Footer";
+import "aos/dist/aos.css";
+import AOS from "aos"; 
+import { useEffect } from "react";
+
+
+  
 
 const Root = () => {
+    
+    // Initialize AOS
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with your desired options
+  }, []);
+
+
+
     return (
-        <div>
+        <div data-aos="fade-up">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
